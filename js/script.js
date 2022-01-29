5fe93200725b75ae57bf754bd6941aa58fd2cc9a
Piotrek <ppiatek.5@o2.pl>
2022-01-29 16:52:53 +0100

    Dodanie mechanizmu czatu pomiedzy użytkownikami platformy VOD (użycie wzorca Mediator); klasa Chatroom jest mediatorem, który jest odpowiedzialny za prawidłowe rozsyłanie wiadomości dalej, które otrzyma od użytkowników

--- js/script.js
+++ js/script.js
@@ -95,3 +95,89 @@ const newmovies = [
 ]
 
 newmovies.forEach( m => { m.define()})
+
+
+//mediator design pattern
+
+class User {
+  constructor(nick) {
+    this.nick = nick; 
+    this.chatroom = null; 
+  }
+
+  addToChatroom(chatroom) {
+    // za pomocą tej metody może dołączyć do czatu
+    this.chatroom = chatroom;
+  }
+
+  sendMessage(message, to) {
+    // może wysłać wiadomość - trafi ona do mediatora
+    this.chatroom.send(message, this, to);
+  }
+
+  receive(message, from, type) {
+    // może wyświetlić otrzymaną wiadomość oraz jej nadawcę
+    // wiadomość otrzymamy od mediatora
+    console.log(
+      `[Do ${this.nick}] Nowa ${type} wiadomość od ${from.nick}: ${message}`
+    );
+  }
+}
+
+// "Chatroom" to mediator w naszym systemie
+// o on będzie odbierał i wysyłał dalej wiadomości
+class Chatroom {
+  constructor() {
+    this.participants = {}; // mediator wie o wszystkich uczestnikach czatu
+  }
+
+  join(participant) {
+
+    this.participants[participant.nick] = participant;
+
+    participant.addToChatroom(this);
+  }
+
+  /* 
+      Mediator jest odpowiedzilany za prawidłowe rozsyłanie dalej
+      wiadomości które otrzyma od użytkowników.
+      Możemy dodać tutaj wiele funkcjonalności jak np.
+      filtrowanie niecenzuralnych słów, szyfrowanie/deszyfrowanie, itd.  
+  */
+  send(message, from, to) {
+    if (to) {
+      // jeżeli sprecyzujemy odbiorcę,
+      // mediator wyśle to jako wiadomość prywatną
+      to.receive(message, from, "prywatna");
+    } else {
+      // w przeciwnym wypadku wiadomośc trafi na czat grupowy
+      // tzn. zostanie wysłana do wszystkich
+      for (const key in this.participants) {
+        if (this.participants[key] !== from) {
+          this.participants[key].receive(message, from, "publiczna");
+        }
+      }
+    }
+  }
+}
+// Tworzymy instancję naszego mediatora
+const chatroom = new Chatroom();
+
+// Tworzymy uczestników czatu
+const jan = new User("Jan");
+const kamil = new User("Kamil");
+const aga = new User("Aga");
+const piotr = new User("Piotr");
+
+// użytkownicy czatu dołączają do pokoju czatu
+chatroom.join(jan);
+chatroom.join(kamil);
+chatroom.join(aga);
+chatroom.join(piotr);
+
+// i wysyłają na czacie wiadomości
+jan.sendMessage("Czy znacie jakieś fajne filmy?");
+jan.sendMessage("Jakies nowe seriale?"); // wiadomość grupowa
+kamil.sendMessage("Hej, prywatnie mogę Ci polecić Wiedzmina", jan);
+aga.sendMessage("Nuda straszna");
+piotr.sendMessage("Aga, chyba nie ten kanał.", aga); // wiadomość prywatna
